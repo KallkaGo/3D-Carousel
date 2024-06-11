6a79706d9dcbf900bcff2af5f1c7780726909554
KallkaGo <765830637@qq.com>
2024-06-11 17:11:56 +0800

    feat: remove uv scale

--- src/three/components/shaders/vertex.glsl
+++ src/three/components/shaders/vertex.glsl
@@ -14,9 +14,9 @@ void main() {
 
   /* scale */
   p *= (1. + .2 * uDisCenter);
-  uv2 = uv2 * 2. - 1.;
-  uv2 *= (1. + .2 * uDisCenter) * .6;
-  uv2 = (uv2 + 1.) * .5;
+  // uv2 = uv2 * 2. - 1.;
+  // uv2 *= (1. + .2 * uDisCenter) * .6;
+  // uv2 = (uv2 + 1.) * .5;
   vec4 modelPosition = modelMatrix * vec4(p, 1.);
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
